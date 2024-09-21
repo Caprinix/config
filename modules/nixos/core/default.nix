4e7ab5005e8165cc22b4421540c487e69487a61f
replicapra <replicapra@outlook.com>
2024-09-21 18:10:32 +0200

    feat: add some default programs

--- modules/nixos/core/default.nix
+++ modules/nixos/core/default.nix
@@ -1,11 +1,30 @@
-{ ... }:
+{ pkgs, lib, ... }:
 let
+  inherit (lib.caprinix) enabled;
 
+  htop-settings = import ./htop-settings.nix;
 in
 {
   imports = [
     ./nix.nix
   ];
 
-  config = { };
+  config = {
+    environment.systemPackages = with pkgs; [
+      ripgrep
+      ripgrep-all
+      jq
+      fq
+      yq
+      jnv
+      unrar
+      unzip
+    ];
+
+    programs = {
+      htop = enabled // {
+        settings = htop-settings;
+      };
+    };
+  };
 }
---
+++ modules/nixos/core/htop-settings.nix
@@ -0,0 +1,30 @@
+{
+  detailed_cpu_time = true;
+  header_layout = "two_50_50";
+  column_meters_0 = [
+    "Hostname"
+    "DateTime"
+    "Uptime"
+    "LoadAverage"
+  ];
+  column_meter_modes_0 = [
+    2
+    2
+    2
+    2
+  ];
+  column_meters_1 = [
+    "AllCPUs2"
+    "MemorySwap"
+    "DiskIO"
+    "NetworkIO"
+  ];
+  column_meter_modes_1 = [
+    1
+    1
+    1
+    1
+  ];
+  tree_view = true;
+  tree_view_always_by_pid = true;
+}
