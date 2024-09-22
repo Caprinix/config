6971c5331e259f0f1caea42d61c1e437918a4d33
replicapra <replicapra@outlook.com>
2024-09-22 12:25:58 +0200

    feat: add zsh

---
+++ modules/home/programs/zsh/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  inherit (lib) mkEnableOption;
+  inherit (lib.caprinix) mkIfEnabled enabled;
+
+  cfg = config.caprinix.programs.zsh;
+in
+{
+  options.caprinix.programs.zsh = {
+    enable = mkEnableOption "zsh";
+  };
+
+  config = mkIfEnabled cfg {
+    programs = {
+      zsh = {
+        enable = true;
+        enableCompletion = true;
+        history = {
+          ignoreSpace = true;
+          path = "${config.xdg.dataHome}/zsh/zshistory";
+        };
+        syntaxHighlighting = enabled // {
+          highlighters = [
+            "main"
+            "brackets"
+          ];
+        };
+        oh-my-zsh = enabled // {
+          extraConfig = ''
+            zstyle ':omz:update' mode auto
+            zstyle ':omz:update' frequency 1
+            	  '';
+          plugins = [ ];
+        };
+      };
+    };
+  };
+}
