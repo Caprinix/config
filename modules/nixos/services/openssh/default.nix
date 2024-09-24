1baeb2fbb0cb771ae726df3d7ea5e5835b1f56f7
replicapra <replicapra@outlook.com>
2024-09-24 23:11:30 +0200

    feat: add openssh service with fail2ban

---
+++ modules/nixos/services/openssh/default.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkEnableOption mkIf;
+  inherit (lib.caprinix) enabled;
+
+  cfg = config.caprinix.services.openssh;
+in
+{
+  options.caprinix.services.openssh = {
+    enable = mkEnableOption "openssh";
+  };
+
+  config = mkIf cfg.enable {
+    services = {
+      openssh = enabled // {
+        settings = {
+          X11Forwarding = false;
+          UseDns = true;
+          PermitRootLogin = "no";
+          PasswordAuthentication = false;
+        };
+        extraConfig = ''
+          PubkeyAuthentication yes
+          PermitEmptyPasswords no
+          ChallengeResponseAuthentication no
+          PermitUserEnvironment no
+          AllowAgentForwarding no
+          AllowTcpForwarding no
+          PermitTunnel no
+          ClientAliveInterval 300
+        '';
+      };
+      fail2ban = enabled;
+    };
+  };
+}
