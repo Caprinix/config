749b69fb0f3873ca5771eecc30707656cab0778b
replicapra <replicapra@outlook.com>
2024-10-24 19:10:54 +0200

    feat: add devenv to workspace

---
+++ devenv.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  languages.nix.enable = true;
+  languages.nix.lsp.package = pkgs.nixd;
+
+  editors.vscode.enable = true;
+  editors.vscode.mixins = [ "nix" ];
+}
