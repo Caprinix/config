991feab4b5dd17257e63f17a49228c92cb533ac3
replicapra <replicapra@outlook.com>
2024-12-16 15:47:23 +0100

    feat: add system and home for charming-wise-oak

---
+++ homes/x86_64-linux/replicapra@charming-wise-oak/default.nix
@@ -0,0 +1,17 @@
+{lib, ...}: let
+  inherit (lib.caprinix) enabled;
+in {
+
+  config = {
+    caprinix = {
+      programs = {
+        atuin = enabled;
+        git = enabled;
+        neovim = enabled;
+        starship = enabled;
+        tmux = enabled;
+        zsh = enabled;
+      };
+    };
+  };
+}
