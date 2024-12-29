9a9f9f78907b310bfcbdd72481266cd5ae42a223
replicapra <replicapra@outlook.com>
2024-12-29 22:34:01 +0100

    feat: rename ethereal-creative-satyr
    renamed to ethereal-inspired-satyr to avoid having an e-c-s and e-c-d config

--- lib/systems/ethereal-creative-satyr.nix
+++
@@ -1,3 +0,0 @@
-{
-  sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFWofLbCSSo8ltt7PdeV3eERPS3AAztBvoRbUjKTWGmo replicapra@ethereal-creative-satyr";
-}
---
+++ lib/systems/ethereal-inspired-satyr.nix
@@ -0,0 +1,4 @@
+{
+  sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHAJh12l5rlcF1lprlENLGV8KbqSiJQK2fX0PIXukFRL replicapra@ethereal-inspired-satyr
+";
+}
