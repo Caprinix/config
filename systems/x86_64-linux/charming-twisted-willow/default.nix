f0b369c383255528a3e5832d0169ed9ef3b476f5
Lucian Kath <154707993+replicapra@users.noreply.github.com>
2025-01-19 10:45:34 +0100

    feat: add charming-twisted-willow (#17)
    * chore(inputs): update flake inputs
    
    * feat: add home and system for charming-twisted-willow

---
+++ systems/x86_64-linux/charming-twisted-willow/default.nix
@@ -0,0 +1,34 @@
+{lib, ...}: let
+  inherit (lib.caprinix) enabled systems;
+in {
+  imports = [
+    ./configuration.nix
+    ./hardware-configuration.nix
+  ];
+
+  config = {
+    caprinix = {
+      disko =
+        enabled
+        // {
+          layout = "impermanence";
+          args = {
+            device = "/dev/sda";
+          };
+        };
+      impermanence = enabled;
+      persistence = enabled;
+      deployment = enabled;
+      services = {
+        openssh =
+          enabled
+          // {
+            authorizedKeys = [
+              systems.wandering-woof-scorebook.sshPublicKey
+              systems.ethereal-inspired-satyr.sshPublicKey
+            ];
+          };
+      };
+    };
+  };
+}
