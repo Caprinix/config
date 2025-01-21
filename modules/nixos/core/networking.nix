84584979226e5bb1bdc53e1954d42b28a7583850
replicapra <replicapra@outlook.com>
2025-01-21 20:05:19 +0100

    feat: enable nftables in core module

--- modules/nixos/core/networking.nix
+++ modules/nixos/core/networking.nix
@@ -1,8 +1,12 @@
-_: {
+{lib, ...}: let
+  inherit (lib.caprinix) enabled;
+in {
   config = {
     networking.useNetworkd = true;
     networking.domain = "replicapra.dev";
 
     systemd.network.enable = true;
+
+    networking.nftables = enabled;
   };
 }
