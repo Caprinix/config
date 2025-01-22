ea17476d4099324d9765f3e196a22ca50909b263
replicapra <replicapra@outlook.com>
2025-01-22 16:04:01 +0100

    feat: add scaly-vocal-goat home and system configuration

---
+++ systems/x86_64-linux/scaly-vocal-goat/default.nix
@@ -0,0 +1,40 @@
+{lib, ...}: let
+  inherit (lib.caprinix) enabled systems;
+in {
+  imports = [
+    ./configuration.nix
+    ./hardware-configuration.nix
+  ];
+
+  config = {
+    caprinix = {
+      disko =
+        enabled
+        // {
+          layout = "impermanence";
+          args = {
+            device = "/dev/sda";
+          };
+        };
+      impermanence = enabled;
+      persistence = enabled;
+      deployment = enabled;
+      services = {
+        tailscale = enabled;
+        openssh =
+          enabled
+          // {
+            authorizedKeys = [
+              systems.wandering-woof-scorebook.sshPublicKey
+              systems.ethereal-inspired-satyr.sshPublicKey
+            ];
+          };
+      };
+      hetzner =
+        enabled
+        // {
+          ipv6 = "2a01:4f8:c0c:8143::1/64";
+        };
+    };
+  };
+}
