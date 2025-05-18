beb82f4b97c71b5f7496bfd57c7ac6179b2e644e
Lucian Kath <154707993+replicapra@users.noreply.github.com>
2025-05-18 18:26:09 +0200

    feat: add home-manager (#3)
    * chore(inputs): add home-manager
    
    * feat: add home-manager module to NixOS systems configuration
    
    * feat: set core configs for home-manager

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
     snowfall-lib.url = "github:snowfallorg/lib";
     snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
 
+    # region nix-community
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     #region misc
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
@@ -30,6 +34,11 @@
     };
   in
     lib.mkFlake {
+
+       systems.modules.nixos = with inputs; [
+        home-manager.nixosModules.home-manager
+      ];
+
       outputs-builder = channels: let
         treefmtEval = inputs.treefmt-nix.lib.evalModule channels.nixpkgs ./treefmt.nix;
       in {
