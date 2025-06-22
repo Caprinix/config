cbd2bbbf8acc5037172a34a572522a3a5bd48b7d
replicapra <replicapra@outlook.com>
2025-06-22 16:02:19 +0200

    chore(inputs): add home-manager

--- flake.nix
+++ flake.nix
@@ -6,6 +6,9 @@
 
     snowfall-lib.url = "github:snowfallorg/lib";
     snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
