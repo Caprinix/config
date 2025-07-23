58c766fccab4c5c9a688e68da95d6581483f6a20
replicapra <replicapra@outlook.com>
2025-07-23 23:35:28 +0200

    feat: add impermanence disko layout

---
+++ modules/nixos/disko/layouts/impermanence.nix
@@ -0,0 +1,81 @@
+{device ? "/dev/sda", ...}: {
+  disko.devices = {
+    disk = {
+      main = {
+        inherit device;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
+            };
+            esp = {
+              name = "ESP";
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            swap = {
+              size = "8G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            };
+            root = {
+              name = "root";
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "root_vg";
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      root_vg = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "btrfs";
+              extraArgs = ["-f"];
+
+              subvolumes = {
+                "/root" = {
+                  mountpoint = "/";
+                };
+
+                "/persistent" = {
+                  mountOptions = [
+                    "subvol=persistent"
+                    "noatime"
+                  ];
+                  mountpoint = "/persistent";
+                };
+
+                "/nix" = {
+                  mountOptions = [
+                    "subvol=nix"
+                    "noatime"
+                  ];
+                  mountpoint = "/nix";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
