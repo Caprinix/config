166e750a116cf2458a64a5f39ec07b7e1ef136b6
replicapra <154707993+replicapra@users.noreply.github.com>
2025-07-26 21:33:04 +0200

    feat: add some more defaults to home-manager core module

--- modules/home/core/default.nix
+++ modules/home/core/default.nix
@@ -1,5 +1,18 @@
-{lib, ...}: let
+{
+  lib,
+  osConfig,
+  ...
+}: let
   inherit (lib.snowfall.fs) get-non-default-nix-files;
+  inherit (lib.caprinix-essentials.modules) enabled;
 in {
   imports = get-non-default-nix-files ./.;
+
+  config = {
+    home.shell.enableShellIntegration = true;
+    home.stateVersion = osConfig.system.stateVersion or "25.05";
+
+    programs.home-manager = enabled;
+    programs.nix-your-shell = enabled;
+  };
 }
