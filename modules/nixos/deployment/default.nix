9f8447a8fdae161a3b5005a489e27e43389385f1
replicapra <154707993+replicapra@users.noreply.github.com>
2025-07-27 20:49:31 +0200

    feat: create NixOS deployment module with user declaration

---
+++ modules/nixos/deployment/default.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) mkEnableOption;
+  inherit (lib.caprinix-essentials.modules) mkIfEnabled;
+
+  cfg = config.caprinix.deployment;
+in {
+  options.caprinix.deployment = {
+    enable = mkEnableOption "deployment";
+  };
+
+  config = mkIfEnabled cfg {
+    users.groups.deployment = {};
+    users.users.deployment = {
+      isNormalUser = true;
+      group = "deployment";
+      extraGroups = [
+        "wheel"
+      ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxakIX6l4Doi/QPTI884nLWyBeaPN96qcp46iUEuUa3 deployment@caprinix"
+      ];
+    };
+  };
+}
